3739c1093a266136e69f1e8f2269684077dfa504
Guillermo Rodriguez <guillermordrgzacosta@gmail.com>
2021-12-04 14:13:31 -0300

    Cinnalovers - Funciones Js

---
+++ js/calculadora.js
@@ -0,0 +1,24 @@
+const precioMaxis = 230
+const cantidadRolls = +prompt('Tienes descuentos comprando más de 3 maxi rolls! Ingresa el número de maxi rolls para calcular tu descuento!')
+
+if (cantidadRolls >= 3){
+    descuento = 0,85
+} else{
+    alert("No tienes descuento, son a partir de 3")
+}
+
+if (cantidadRolls >= 6){
+    descuento = 0.80
+} else{}
+
+if (cantidadRolls >=12){
+    descuento = 0.75
+} else{}
+
+function resultado(){
+    let subtotal = precioMaxis * cantidadRolls;
+    let total = subtotal * descuento;
+    let ahorro = subtotal - total;
+    alert("Tienes un descuento de $" + ahorro + ", tu total es $" + total)
+}
+resultado()
