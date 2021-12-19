0b4be013a37777cc426c3410bd66a8e5a1dd4766
Guillermo Rodriguez <guillermordrgzacosta@gmail.com>
2021-12-18 23:51:40 -0300

    Desafio DOM

--- js/carrito.js
+++ js/carrito.js
@@ -1,68 +1,184 @@
-class Carrito {
-
-    constructor( lista ) {
-        this.lista = lista;
-    }
-
-    findAllItems() {
-        return this.lista;
-    }
-
-    createItem( item ) {
-        this.lista.push( item )
-    }
-
-    findOneByIdItem( itemId ) {
-        const item = this.lista.find( element => element.id === itemId )
-
-        if ( !item ) {
-            throw new Error('No existe el item id:' + itemId)
-        }
-
-        return item;
-    }   
-}
 class Producto {
-    constructor(id, nombre, precio, cantidad, stock) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
-        this.stock = stock;
-    }
+  constructor(id, nombre, precio, stock, img) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.img = img;
+  }
+}
+const rollClasico = new Producto(
+  1,
+  "Roll Clásico",
+  230,
+  100,
+  "./images/toppings/toppingClasico.jpg"
+);
+const rollNutella = new Producto(
+  2,
+  "Roll Nutella",
+  280,
+  50,
+  "./images/toppings/toppingNutella.jpg"
+);
+const rollDulce = new Producto(
+  3,
+  "Roll DDL",
+  260,
+  100,
+  "./images/toppings/toppingDulce.jpg"
+);
+const rollPecan = new Producto(
+  4,
+  "Roll Caramel & Pecans",
+  270,
+  50,
+  "./images/toppings/toppingPecan.jpg"
+);
+const rollManzana = new Producto(
+  5,
+  "Roll Manzana",
+  270,
+  50,
+  "./images/toppings/toppingManzana.jpg"
+);
+const rollFrutos = new Producto(
+  6,
+  "Roll Frutos Rojos",
+  270,
+  100,
+  "./images/toppings/toppingFrutos.jpg"
+);
+const rollChoco = new Producto(
+  7,
+  "Roll Chocolate",
+  260,
+  100,
+  "./images/toppings/toppingChoco.jpg"
+);
 
-    getNombre() {
-        return this.nombre;
-    }
+let misProductos = [];
 
-    getPrecio() {
-        return this.precio;
-    }
+misProductos.push(
+  rollClasico,
+  rollNutella,
+  rollDulce,
+  rollPecan,
+  rollManzana,
+  rollFrutos,
+  rollChoco
+);
 
-    agotado() {
-        this.stock = false;
-    }
+let contenedorProductos = document.getElementById("productos");
+
+for (let producto of misProductos) {
+  if (producto.stock > 0) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "producto");
+
+    div.innerHTML = `
+<img src="${producto.img}" alt="" width="150" class="rounded-circle px-2">
+<button type="button" class="btn btn-dark" id="producto${producto.id}">${producto.nombre}</button>
+`;
+
+    contenedorProductos.appendChild(div);
+  } else {
+    let div = document.createElement("div");
+    div.setAttribute("class", "producto");
+
+    div.innerHTML = `
+<img src="${producto.img}" alt="" width="150">
+<button type="button" class="button" id="producto${producto.id}">sin stock</button>
+`;
+
+    contenedorProductos.appendChild(div);
+  }
 }
 
-const rollClasico = new Producto(1, 'Roll Clásico', 230, 100, true);
-const rollNutella = new Producto(2, 'Roll Nutella', 280, 50, true);
-const rollDulce = new Producto(3, 'Roll DDL', 260, 100, true);
-const rollPecan = new Producto(4, 'Roll Caramel & Pecans', 270, 50, true);
-const rollManzana = new Producto(5, 'Roll Manzana', 270, 50, true);
-const rollFrutos = new Producto(6, 'Roll Frutos Rojos', 270, 100, true);
-const rollChoco = new Producto(7, 'Roll Chocolate', 260, 100, true);
-const rollPerso = new Producto(8, prompt('Personaliza tu roll'), 280, 1, true)
-
-const carrito = new Carrito ([])
-carrito.createItem (rollClasico)
-carrito.createItem (rollNutella)
-carrito.createItem (rollDulce)
-carrito.createItem (rollPecan)
-carrito.createItem (rollManzana)
-carrito.createItem (rollFrutos)
-carrito.createItem (rollChoco)
-carrito.createItem (rollPerso)
-
-console.log( carrito.findAllItems() )
-alert("Tu roll personalizado es " + rollPerso.getNombre())
-console.log( rollPerso.getNombre() )
+let roll1 = document.getElementById("producto1");
+producto1.addEventListener("click", function producto1() {
+  document.getElementById("lista").innerHTML +=
+    "<li>" +
+    misProductos[0].nombre +
+    " - $ " +
+    misProductos[0].precio +
+    "</li>";
+  for (let i = 0; i <= lista.children.length - 1; i++) {
+    lista.children[i].addEventListener("click", eliminarProducto);
+  }
+});
+let roll2 = document.getElementById("producto2");
+producto2.addEventListener("click", function producto2() {
+  document.getElementById("lista").innerHTML +=
+    "<li>" +
+    misProductos[1].nombre +
+    " - $ " +
+    misProductos[1].precio +
+    "</li>";
+  for (let i = 0; i <= lista.children.length - 1; i++) {
+    lista.children[i].addEventListener("click", eliminarProducto);
+  }
+});
+let roll3 = document.getElementById("producto3");
+producto3.addEventListener("click", function producto3() {
+  document.getElementById("lista").innerHTML +=
+    "<li>" +
+    misProductos[2].nombre +
+    " - $ " +
+    misProductos[2].precio +
+    "</li>";
+  for (let i = 0; i <= lista.children.length - 1; i++) {
+    lista.children[i].addEventListener("click", eliminarProducto);
+  }
+});
+let roll4 = document.getElementById("producto4");
+producto4.addEventListener("click", function producto4() {
+  document.getElementById("lista").innerHTML +=
+    "<li>" +
+    misProductos[3].nombre +
+    " - $ " +
+    misProductos[3].precio +
+    "</li>";
+  for (let i = 0; i <= lista.children.length - 1; i++) {
+    lista.children[i].addEventListener("click", eliminarProducto);
+  }
+});
+let roll5 = document.getElementById("producto5");
+producto5.addEventListener("click", function producto5() {
+  document.getElementById("lista").innerHTML +=
+    "<li>" +
+    misProductos[4].nombre +
+    " - $ " +
+    misProductos[4].precio +
+    "</li>";
+  for (let i = 0; i <= lista.children.length - 1; i++) {
+    lista.children[i].addEventListener("click", eliminarProducto);
+  }
+});
+let roll6 = document.getElementById("producto6");
+producto6.addEventListener("click", function producto6() {
+  document.getElementById("lista").innerHTML +=
+    "<li>" +
+    misProductos[5].nombre +
+    " - $ " +
+    misProductos[5].precio +
+    "</li>";
+  for (let i = 0; i <= lista.children.length - 1; i++) {
+    lista.children[i].addEventListener("click", eliminarProducto);
+  }
+});
+let roll7 = document.getElementById("producto7");
+producto7.addEventListener("click", function producto7() {
+  document.getElementById("lista").innerHTML +=
+    "<li>" +
+    misProductos[6].nombre +
+    " - $ " +
+    misProductos[6].precio +
+    "</li>";
+  for (let i = 0; i <= lista.children.length - 1; i++) {
+    lista.children[i].addEventListener("click", eliminarProducto);
+  }
+});
+let eliminarProducto = function eliminarProducto() {
+    this.parentNode.removeChild(this);
+}
